d07c731fc7b37e5ca3cab849054c847d783c4203
pavels64 <125793235+pavels64@users.noreply.github.com>
2023-03-14 17:05:47 +0000

    pk1

--- build_heap.py
+++ build_heap.py
@@ -4,34 +4,44 @@
 def build_heap(data):
     swaps = []
     n = len(data)
-    for i in range(n // 2 - 1, -1, -1):
-        j = i
-        while 2 * j + 1 < n:
-            k = 2 * j + 1
-            if k + 1 < n and data[k + 1] < data[k]:
-                k += 1
-            if data[j] > data[k]:
-                data[j], data[k] = data[k], data[j]
-                swaps.append((j, k))
-                j = k
-            else:
-                break
+    # Perform heapify operation on every non-leaf node
+    for i in range(n//2-1, -1, -1):
+        min_heapify(data, i, n, swaps)
     return swaps
+
+def min_heapify(data, i, n, swaps):
+    # Find the indices of the left and right children
+    left_child = 2*i + 1
+    right_child = 2*i + 2
+    # Find the index of the smallest node among the current node and its children
+    smallest = i
+    if left_child < n and data[left_child] < data[smallest]:
+        smallest = left_child
+    if right_child < n and data[right_child] < data[smallest]:
+        smallest = right_child
+    # If the smallest node is not the current node, swap them and continue heapifying
+    if smallest != i:
+        swaps.append((i, smallest))
+        data[i], data[smallest] = data[smallest], data[i]
+        min_heapify(data, smallest, n, swaps)
+
 
 
 def main():
-    with open('input.txt', 'r') as f:
-        n = int(f.readline())
-        data = list(map(int, f.readline().split()))
+    # Read input from user
+    n = int(input().strip())
+    data = list(map(int, input().strip().split()))
 
-    assert len(data) == n
+    # Check input constraints
+    assert n == len(data)
 
+    # Call build_heap to get swap operations
     swaps = build_heap(data)
 
+    # Check output constraints
+    assert len(swaps) <= 4 * n
+
+    # Print number of swaps and swap operations
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
-
-if __name__ == "__main__":
-    main()
