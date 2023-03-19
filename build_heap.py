f72970c7cbc852d7e7e96116a382ae63eb4de491
pavels64 <125793235+pavels64@users.noreply.github.com>
2023-03-19 20:12:11 +0000

    pk1

--- build_heap.py
+++ build_heap.py
@@ -1,47 +1,101 @@
 # python3
 
 
+def keyboard():
+    try:
+        n = int(input().strip())
+        if n <= 0:
+            return None
+        
+        data = list(map(int, input().strip().split(" ")))
+        return data
+    
+    except ValueError:
+        print("Invalid input: n must be a positive integer")
+        return None
+
+
+
+def file(filename):
+    try:
+        with open(f"./tests/{filename}") as f:
+            contents = f.readlines()
+    except FileNotFoundError:
+        print("File not found")
+        return None
+    except:
+        print("Error reading file")
+        return None
+    
+    n = contents[0].strip()
+    if not n:
+        print("Invalid input: n not provided")
+        return None
+    
+    data = list(map(int, contents[1].strip().split(" ")))
+    if not data:
+        print("Invalid input: data not provided")
+        return None
+    
+    return data
+
+
+def sift_down(data, i, swaps):
+    min_index = i
+    
+    left = 2 * i + 1
+    if left < len(data):
+        _, min_index = min(enumerate([data[left], data[min_index]]), key=lambda x: x[1])
+        if min_index != i:
+            swaps.append((i, min_index))
+            data[i], data[min_index] = data[min_index], data[i]
+            sift_down(data, min_index, swaps)
+    
+    right = 2 * i + 2
+    if right < len(data):
+        _, min_index = min(enumerate([data[right], data[min_index]]), key=lambda x: x[1])
+        if min_index != i:
+            swaps.append((i, min_index))
+            data[i], data[min_index] = data[min_index], data[i]
+            sift_down(data, min_index, swaps)
+
+
 def build_heap(data):
     swaps = []
-    n = len(data)
-    # Perform heapify operation on every non-leaf node
-    for i in range(n//2-1, -1, -1):
-        min_heapify(data, i, n, swaps)
-    return swaps
-
-def min_heapify(data, i, n, swaps):
-    # Find the indices of the left and right children
-    left_child = 2*i + 1
-    right_child = 2*i + 2
-    # Find the index of the smallest node among the current node and its children
-    smallest = i
-    if left_child < n and data[left_child] < data[smallest]:
-        smallest = left_child
-    if right_child < n and data[right_child] < data[smallest]:
-        smallest = right_child
-    # If the smallest node is not the current node, swap them and continue heapifying
-    if smallest != i:
-        swaps.append((i, smallest))
-        data[i], data[smallest] = data[smallest], data[i]
-        min_heapify(data, smallest, n, swaps)
+    nulle = 0
+    i = len(data) // 2
+    while i >= 0:
+        sift_down(data, i, swaps)
+        nulle = max(nulle, len(swaps))
+        i -= 1
+    return nulle, swaps
 
 
 
 def main():
-    # Read input from user
-    n = int(input().strip())
-    data = list(map(int, input().strip().split()))
+    input_method = input().strip()
 
-    # Check input constraints
-    assert n == len(data)
+    if input_method == "I":
+        data = keyboard()
+        if data:
+            nulle, swaps = build_heap(data)
+            print(nulle)
 
-    # Call build_heap to get swap operations
-    swaps = build_heap(data)
+            for i, j in swaps:
+                print(i, j)
 
-    # Check output constraints
-    assert len(swaps) <= 4 * n
+    elif input_method == "F":
+        filename = input().strip()
+        if str(filename[-1]) != "a":
+            data = file(filename)
+            if data:
+                nulle, swaps = build_heap(data)
+                print(nulle)
+                
+                for i, j in swaps:
+                    print(i, j)
+        else:
+            print("Unable to build heap from file.")
 
-    # Print number of swaps and swap operations
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
+if __name__ == "__main__":
+    main()
